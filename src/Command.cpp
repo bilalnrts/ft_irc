73f9c8d6f9aa6c4306a5b89fc45b15eee6bef26c
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-10-10 00:25:42 +0300

    check operations removed in pass func

--- src/Command.cpp
+++ src/Command.cpp
@@ -27,36 +27,12 @@ namespace Command
 		}
 	}
 
-	/*
-		Command: PASS
-		Parameters: <password>
-		NUMERIC REPLIES :
-		-ERR_NEEDMOREPARAMS		->	"<command> :Not enough parameters"				->	Returned by the server by numerous commands to
-																						indicate to the client that it didn't supply enough parameters.
-		-ERR_ALREADYREGISTRED	->	":Unauthorized command (already registered)"	->	Returned by the server to any link which tries to
-																						change part of the registered details (such as
-																						password or user details from second USER message).
-		EXAMPLE :
-			PASS secretpasswordhere
-	*/
 	void	pass(int fd, Server *server, std::string msg) //TOPLAM 2 HATA HANDLE EDİLECEK
 	{
 		User *user = server->findUser(fd);
 		std::vector<std::string> spl = utils::split(msg, " ");
-		std::string pass;
 
-		pass = spl[1];
-		if (!pass.empty() && pass[pass.length() - 1] == '\n') {         pass.erase(pass.length() - 1);     }
-		std::cout << "msg geldi : " + msg << " pass : " + pass << std::endl;
-		if (spl.size() < 2)
-		{
-			//NUMERIC REPLY -> ERR_NEEDMOREPARAMS
-			std::cout << "Password yok" << std::endl;
-		}
-		std::cout << "ser pass : " + server->getPassword() << std::endl;
-		std::cout << "ser type : " << typeid(server->getPassword()).name() << std::endl;
-		std::cout << "pass type : " << typeid(pass).name() << std::endl;
-		if (pass == server->getPassword()) {
+		if (check::pass(spl[0], spl[1], spl.size(), user, server)) {
 			user->setAuths("PASS", true);
 			std::cout << "True burasi" << std::endl;
 		}
