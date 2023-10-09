41c37364cdff7264400907c43e7abea1a3ea6ceb
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-10-10 01:52:21 +0300

    done execute user func

--- src/Command.cpp
+++ src/Command.cpp
@@ -27,34 +27,33 @@ namespace Command
 		}
 	}
 
-	void	pass(int fd, Server *server, std::string msg) //TOPLAM 2 HATA HANDLE EDİLECEK
+	void	pass(int fd, Server *server, std::string msg)
 	{
 		User *user = server->findUser(fd);
 		std::vector<std::string> spl = utils::split(msg, " ");
 
 		if (check::pass(spl[0], spl[1], spl.size(), user, server)) {
 			user->setAuths("PASS", true);
-			std::cout << "True burasi" << std::endl;
 		}
 	}
 
-	/*
-		Command: USER
-		Parameters: <user> <mode> <unused> <realname>
-		NUMERIC REPLIES :
-		-ERR_NEEDMOREPARAMS		->	"<command> :Not enough parameters"				->	Returned by the server by numerous commands to
-																						indicate to the client that it didn't supply enough parameters.
-		-ERR_ALREADYREGISTRED	->	":Unauthorized command (already registered)"	->	Returned by the server to any link which tries to
-																						change part of the registered details (such as
-																						password or user details from second USER message).
-		EXAMPLE :
-			USER guest 0 * :Ronnie Reagan
-	*/
-	void	user(int fd, Server *server, std::string msg) // YORULDUM SALIYORUM VALLA
+
+	void	user(int fd, Server *server, std::string msg)
 	{
-		(void)fd;
-		(void)server;
-		(void)msg;
-		std::cout << "User komutu calisti" << std::endl;
+		User *user = server->findUser(fd);
+		std::vector<std::string> splWithColon = utils::split(msg, ":");
+		std::vector<std::string> splWithSpace = utils::split(msg, " ");
+
+		if (check::user(splWithColon, splWithSpace, user, server, fd)) {
+			user->setUsername(splWithSpace[1]);
+			user->setRealname(splWithColon[1]);
+			user->setMode(std::stoi(splWithSpace[2], 0, 10));
+			user->setHostname(splWithSpace[3]);
+			user->setAuths("USER", true);
+		}
+		std::cout << "Nick : " + user->getNickname() << std::endl;
+		std::cout << "Real : " + user->getRealname() << std::endl;
+		std::cout << "Host : " + user->getHostname() << std::endl;
+		std::cout << "Mode : " << user->getMode() << std::endl;
 	}
 }
