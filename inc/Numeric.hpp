08d066ffdeef4ba12d9546b71ce4aadb6504de21
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-10-10 00:28:26 +0300

    added 2 new numeric errors (ERR_NEEDMOREPARAMS(cmd) & ERR_ALREADYREGISTRED)

--- inc/Numeric.hpp
+++ inc/Numeric.hpp
@@ -9,9 +9,12 @@ class Server;
 class User;
 
 // NUMERIC ERRORS
-#define ERR_NICKNAMEINUSE(nick) numeric::makeNumeric("433", nick + " :Nickname is already in use")
-#define ERR_NONICKNAMEGIVEN numeric::makeNumeric("431", ":No nickname given")
-#define ERR_ERRONEUSNICKNAME(nick) numeric::makeNumeric("432", nick + " :Erroneous nickname")
+#define ERR_NICKNAMEINUSE(nick)		numeric::makeNumeric("433", nick + " :Nickname is already in use")
+#define ERR_NONICKNAMEGIVEN			numeric::makeNumeric("431", ":No nickname given")
+#define ERR_ERRONEUSNICKNAME(nick)	numeric::makeNumeric("432", nick + " :Erroneous nickname")
+#define ERR_NEEDMOREPARAMS(cmd)		numeric::makeNumeric("461", cmd + " :Not enough parameters")
+#define ERR_ALREADYREGISTRED		numeric::makeNumeric("462", " :Unauthorized command (already registered)")
+
 namespace numeric
 {
 	Numeric makeNumeric(std::string code, std::string message);
