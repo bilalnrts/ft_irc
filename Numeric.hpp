dc0c1f7914d1d11c0480f4d29f8d78b47bebf7ed
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-10-09 20:50:02 +0300

    added new numeric err (ERR_ERRONEUSNICKNAME)

--- Numeric.hpp
+++ Numeric.hpp
@@ -11,7 +11,7 @@ class User;
 // NUMERIC ERRORS
 #define ERR_NICKNAMEINUSE(nick) numeric::makeNumeric("433", nick + " :Nickname is already in use")
 #define ERR_NONICKNAMEGIVEN numeric::makeNumeric("431", ":No nickname given")
-
+#define ERR_ERRONEUSNICKNAME(nick) numeric::makeNumeric("432", nick + " :Erroneous nickname")
 namespace numeric
 {
 	Numeric makeNumeric(std::string code, std::string message);
